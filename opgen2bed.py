1cc0235d60ffc5a6f8eb560412ef81a867d371a8
Remi-Andre Olsen <remi-andre.olsen@scilifelab.se>
2015-04-15 14:29:14 +0200

    Added: OpGen XML parsing and conversion to bed

---
+++ opgen2bed.py
@@ -0,0 +1,95 @@
+import argparse
+from intervaltree import Interval, IntervalTree
+from itertools import groupby
+from opgenxml import OpGenMapPlacement, OpGenRestrictionMap        
+
+
+parser = argparse.ArgumentParser(
+        description='Parse OpGen XML files and parse opticalmap-insilico digested fasta alignments')
+parser.add_argument('--xml', type=str, help='Opgen XML placement file')
+parser.add_argument('--bed_in', type=str, help='Input bed file to be translated to opgen alignment coordinates')
+parser.add_argument('--bed', type=str, help='Output bed file (default: print to stdout)')
+args = parser.parse_args()
+
+opmaps = OpGenRestrictionMap(args.xml)
+placements = OpGenMapPlacement(args.xml, opmaps)
+bed_out = []
+
+#Target (opgen map) specific information
+tname = placements.align_chunks.keys()[0][0]
+tfrags = opmaps.fragments[tname]
+tchunks = (int(tfrags[0]["I"]), int(tfrags[-1]["I"]))
+tsize = opmaps.fragment_coords(tname, min(tchunks), max(tchunks))[1]
+
+#Annotate regions of fasta file as placed on optical maps
+if args.bed_in is None:
+    for interval in sorted(placements.map_fasta.items()):
+        tstart = interval.begin
+        tend = interval.end
+
+        #Query (in-silico digested sequence) specific information
+        q = interval.data
+        qname = q.data[0]
+        qstrand = q.data[1]
+        bedline = "%s\t%s\t%s\t%s\t%s\t%s" % (tname, tstart, tend, qname, 999, qstrand)
+        bed_out.append(bedline)
+# We are converting bed coordinates to optical map coordintes
+else:
+    bedintervals = IntervalTree()
+    try:
+        with open(args.bed_in) as bi:
+            for bi_line in bi:
+                bi_line = bi_line.split()
+                bedintervals[int(bi_line[1]):int(bi_line[2])] = (bi_line[0], bi_line[3])
+    except IOError as e:
+        print "Something wrong with input bed file: %s" % (e)
+        exit()
+    
+    fmap = {}
+    tree = sorted(placements.fasta_map, key=lambda x: x[2])
+    for k, g in groupby(tree, key=lambda x: x[2]):
+        iv = list(g)
+        if fmap.has_key(k.data):
+            fmap[k.data].update(iv)
+            pass
+        else:
+            fmap[k.data] = IntervalTree(iv)
+    #Bin the bed coordiantes
+    subbeds = {}
+    btree = sorted(bedintervals, key=lambda x: x[2])
+    for k, g in groupby(btree, key=lambda x: x[2]):
+        subbeds[k[0]] = IntervalTree(set(g))
+
+    #Find features that overlap with placed contigs
+    for fasta_key, interval in fmap.iteritems():
+
+        for bedline in subbeds[fasta_key[0]]:
+            feat_start = bedline.begin
+            feat_end = bedline.end
+            overlaps = interval[feat_start:feat_end]
+            # We found overlaps, the feature need to be translated to map-coordinates
+            if overlaps:
+                ctg = list(overlaps)[0]
+                fa_start = ctg.begin
+                fa_end = ctg.end
+                map_start = ctg.data.begin
+                map_end = ctg.data.end
+                if fasta_key[1] == "+":
+                    feat_map_start = map_start + feat_start - fa_start
+                    feat_map_end = map_start + feat_end - fa_start
+                else:
+                    feat_map_start = map_end - feat_start - fa_start
+                    feat_map_end = map_end - feat_end - fa_start
+
+                bed_line =  "%s\t%s\t%s\t%s\t%s" % (
+                        fasta_key[0], feat_map_start, feat_map_end, bedline.data[1], fasta_key[1])
+                bed_out.append(bed_line)
+
+# We're done, writing to file or to stdout
+if args.bed is None:
+    for out_line in bed_out:
+        print out_line
+else:
+    with open(args.bed) as bout:
+        bout.writelines(bed_out)
+
---
+++ opgenxml/__init__.py
@@ -0,0 +1,64 @@
+import xml.etree.ElementTree as ET
+from intervaltree import Interval, IntervalTree
+
+
+
+class OpGenRestrictionMap():
+
+    def __init__(self, xmlfile):
+        self.xmlroot = ET.parse(xmlfile).getroot()
+        self.fragments = {}
+        self._get_fragments()
+
+    def _get_fragments(self):
+        for rmap in self.xmlroot.iter('RESTRICTION_MAP'):
+            mapID = rmap.get('ID').strip()
+            self.fragments[mapID] = [frag.attrib for frag in rmap.iter('F')]
+
+
+    def fragment_coords(self, ID, start, end):
+        fragments = self.fragments[ID]
+        fasta_start = sum([int(frag['S']) for frag in fragments[0:start]])
+        fasta_end = sum([int(frag['S']) for frag in fragments[0:end]])
+
+        return(min(fasta_start,fasta_end),max(fasta_start,fasta_end))
+
+
+class OpGenMapPlacement():
+    
+    def __init__(self, xmlfile, rs_map):
+        self.xmlroot = ET.parse(xmlfile).getroot()
+        self.align_chunks = {}
+        self.map_intervals = IntervalTree()
+        self.fasta_intervals = IntervalTree()
+        self.map_fasta = IntervalTree()
+        self.fasta_map = IntervalTree()
+        self._get_chunks(rs_map)
+
+    def _get_chunks(self, rs_map):
+        for align in self.xmlroot.findall('MAP_ALIGNMENT'):
+            S1 = align.attrib['MAP1'].split()[0]
+            S2 = align.attrib['MAP2'].split()[0]
+
+            chunks = [chunk.attrib for chunk in align.findall('CHUNK')]
+            start_S1 = int(chunks[0]["S1"])
+            start_S2 = int(chunks[0]["S2"])
+            end_S1 = int(chunks[-1]["S1"])
+            end_S2 = int(chunks[-1]["S2"])
+            coords_S1 = rs_map.fragment_coords(S1, start_S1, end_S1)
+            coords_S2 = rs_map.fragment_coords(S2, start_S2, end_S2)
+            if not self.align_chunks.has_key((S1,S2)):
+                self.align_chunks[(S1,S2)] = {coords_S1: chunks}
+            else:
+                self.align_chunks[(S1,S2)][coords_S1] = chunks
+            
+            if start_S2 > end_S2:
+                orientation = "-"
+            else:
+                orientation = "+"
+            self.map_intervals[coords_S1[0]:coords_S1[1]] = (S1,S2)
+            self.fasta_intervals[coords_S2[0]:coords_S2[1]] = S2
+
+            self.map_fasta[coords_S1[0]:coords_S1[1]] = Interval(coords_S2[0], coords_S2[1], (S2, orientation))
+            self.fasta_map[coords_S2[0]:coords_S2[1]] = Interval(coords_S1[0], coords_S1[1], (S2, orientation))
+
